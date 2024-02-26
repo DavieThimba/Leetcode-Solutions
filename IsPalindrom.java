8590802f8fc222c80ce83ed1f419f3a7be3d0312
david <dthimba7gmail.com>
2024-02-26 16:30:41 +0300

    Duplicate

--- IsPalindrom.java
+++ IsPalindrom.java
@@ -7,4 +7,11 @@ public class IsPalindrom {
 
         return xString.equals(reversedString);
     }
+
+    public boolean isPalindrome1(int x) {
+        String xString = Integer.toString(x);
+        String reversedString = new StringBuilder(xString).reverse().toString();
+
+        return xString.equals(reversedString);
+    }
 }
